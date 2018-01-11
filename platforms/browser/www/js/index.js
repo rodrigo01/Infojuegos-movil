a126bd85560dd8367b82dff318dc96446bbf658d
Rodrigo Ornelas <rodrigo01@gmail.com>
2018-01-10 16:05:14 -0800

    pruebas de cambio

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -35,7 +35,50 @@ var app = {
     onDeviceReady: function() {
         //app.receivedEvent('deviceready');
 
-        var ref = window.open('http://www.tecsolutionsmx.com/mmcorpo/', '_blank', 'location=no,zoom=no,toolbar=no');
+        var push = PushNotification.init({
+              android: {},
+              browser: {
+                pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+              },
+              ios: {
+                alert: "true",
+                badge: true,
+                sound: 'false'
+              },
+              windows: {}
+        });
+
+        push.on('registration', function(data) {
+          console.log('registration event: ' + data.registrationId);
+
+          var oldRegId = localStorage.getItem('registrationId');
+          if (oldRegId !== data.registrationId) {
+            // Save new registration ID
+            localStorage.setItem('registrationId', data.registrationId);
+            // Post registrationId to your app server as the value has changed
+          }
+
+          var parentElement = document.getElementById('registration');
+          var listeningElement = parentElement.querySelector('.waiting');
+          var receivedElement = parentElement.querySelector('.received');
+
+          listeningElement.setAttribute('style', 'display:none;');
+          receivedElement.setAttribute('style', 'display:block;');
+        });
+
+        push.on('notification', function(data) {
+          console.log('notification event');
+          navigator.notification.alert(
+            data.message,         // message
+            null,                 // callback
+            data.title,           // title
+            'Ok'                  // buttonName
+          );
+        });
+
+        var ref = window.open('https://www.info-juego.es/v2/', '_blank', 'location=no,zoom=no,toolbar=no');
+
+
 
        
     },
--- www/js/index.js
+++ www/js/index.js
@@ -48,7 +48,35 @@ var app = {
               windows: {}
         });
 
-        //var ref = window.open('https://www.info-juego.es/v2/', '_blank', 'location=no,zoom=no,toolbar=no');
+        push.on('registration', function(data) {
+          console.log('registration event: ' + data.registrationId);
+
+          var oldRegId = localStorage.getItem('registrationId');
+          if (oldRegId !== data.registrationId) {
+            // Save new registration ID
+            localStorage.setItem('registrationId', data.registrationId);
+            // Post registrationId to your app server as the value has changed
+          }
+
+          var parentElement = document.getElementById('registration');
+          var listeningElement = parentElement.querySelector('.waiting');
+          var receivedElement = parentElement.querySelector('.received');
+
+          listeningElement.setAttribute('style', 'display:none;');
+          receivedElement.setAttribute('style', 'display:block;');
+        });
+
+        push.on('notification', function(data) {
+          console.log('notification event');
+          navigator.notification.alert(
+            data.message,         // message
+            null,                 // callback
+            data.title,           // title
+            'Ok'                  // buttonName
+          );
+        });
+
+        var ref = window.open('https://www.info-juego.es/v2/', '_blank', 'location=no,zoom=no,toolbar=no');
 
 
 
